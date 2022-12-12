8745eafa01026cc824fc7f942f09aad3f415cbfd
sakib213 <sakibhss438@gmail.com>
2022-12-12 23:18:18 +0600

    my first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion,  } = require('mongodb-legacy');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w8b4lyk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+  try {
+    await client.connect();
+    const productsCollection = client.db('technical-product').collection('products');
+   app.get('/products',async(req,res)=>{
+    const query = {};
+    const cursor = productsCollection.find(query);
+    const products = await cursor.toArray();
+    res.send(products);
+   })
+
+
+  }
+  finally{
+
+  }
+
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/' , (req,res)=>{
+    res.send('Running technical products')
+
+});
+app.listen(port,() =>{
+    console.log('listening to port',port);
+})
